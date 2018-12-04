fe5bd21336fa59ae90755f623e0d536e8745068f
acwhale <csx5800@163.com>
2018-12-04 16:47:54 +0800

    书籍详情

--- src/pages/bookDetail/bookDetail.vue
+++ src/pages/bookDetail/bookDetail.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container ext-content">
         <div class="head" v-if="book">
             <img :src="book.image" alt="">
             <span class="title">{{book.title}}</span>
@@ -7,7 +7,7 @@
         </div>
         <div class="sub-container">
             <span class="headline">短评</span>
-            <div class="comments-container">
+            <div class="comments-container  ">
                 <div v-for="(item,index) of comments.comments" :key="index">
                     <cmp-tag class="ex-tag" :text="item.content"> 
                     <span class="num" slot="after">{{"+" +item.nums}}</span>
@@ -17,8 +17,27 @@
         </div>
         <div class="sub-container" v-if="book">
             <span class="headline">内容简介</span>
-            <span class="content">{{book.summary}}</span>
+            <p class="content"  v-html="bookSummary"></p>
         </div>
+        <div class="sub-container">
+            <span class="headline">书本信息</span>
+            <div class="detail-contaner">
+            <div class="vertical">
+                <div class="description">出版社</div>
+                <div class="description">出版年</div>
+                <div class="description">页数</div>
+                <div class="description">定价</div>
+                <div class="description">装帧</div>
+            </div>
+            <div class="vertical" v-if="book">
+                <div class="">{{book.publisher}}</div>
+                <div class="">{{book.pubdate}}</div>
+                <div class="">{{book.pages}}</div>
+                <div class="">{{book.price}}</div>
+                <div class="">{{book.binding}}</div>
+            </div>
+        </div>
+    </div>
     </div>
 </template>
 <script>
@@ -68,7 +87,12 @@ export default {
     },
     computed:{
         bookSummary:function(){
-            console.log()
+            if(!this.book.summary) {
+                return  ''
+            }
+            let reg = new RegExp('\\\\n','g')
+            let text = this.book.summary.replace(reg,'<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+            return text
         }
     },
     mounted() {
@@ -138,10 +162,33 @@ export default {
     .ex-tag{
         background-color #fffbdd !important
     }
+    .ext-content{
+        margin-bottom 1rem
+    }
     .content{
+        line-height .4rem
+        letter-spacing .01rem
+        font-size .28rem
         text-indent .6rem
         color #000
         font-weight 500
+    }
+    .detail-contaner{
+        width 100%
+        display flex
+        flex-direction row
+        justify-content flex-start
+        margin-top .5rem
+        font-size .28rem
+        color #666
+    }
+    .vertical {
+        display flex
+        flex-direction column
+    }
+    .description{
+        color #999
+        margin-right .3rem
     }
 
 </style>
